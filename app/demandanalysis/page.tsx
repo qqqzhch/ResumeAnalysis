44d44f00da42c806f98b6c4c8dbaa68ce056da3f
qqqzhch <qqqzhch@163.com>
2024-07-13 10:17:02 +0800

    添加招聘需求分析的ui

--- app/demandanalysis/page.tsx
+++ app/demandanalysis/page.tsx
@@ -1,11 +1,73 @@
-import React from 'react';
 
-const page = () => {
+  
+
+  import { Button } from "@/components/ui/button"
+
+  import { Input } from "@/components/ui/input"
+  import { Label } from "@/components/ui/label"
+  import {
+    CornerDownLeft,
+    Mic,
+
+    Paperclip,
+
+  } from "lucide-react"
+
+  import { Textarea } from "@/components/ui/textarea"
+  import {
+    Tooltip,
+    TooltipContent,
+    TooltipTrigger,
+    TooltipProvider,
+  } from "@/components/ui/tooltip"
+  
+  export default function Dashboard() {
     return (
-        <div>
-            demand analysis
+        <div className="relative flex h-full min-h-[50vh] flex-col rounded-xl bg-muted/50 p-4 lg:col-span-2">
+          
+        
+        <div
+          className="relative overflow-hidden rounded-lg border bg-background focus-within:ring-1 focus-within:ring-ring" x-chunk="dashboard-03-chunk-1"
+        >
+             <TooltipProvider>
+          <Label htmlFor="message" className="sr-only">
+            Message
+          </Label>
+          <Textarea
+            id="message"
+            placeholder="Type your message here..."
+            className="min-h-12 resize-none border-0 p-3 shadow-none focus-visible:ring-0"
+          />
+          <div className="flex items-center p-3 pt-0">
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button variant="ghost" size="icon">
+                  <Paperclip className="size-4" />
+                  <span className="sr-only">Attach file</span>
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent side="top">Attach File</TooltipContent>
+            </Tooltip>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button variant="ghost" size="icon">
+                  <Mic className="size-4" />
+                  <span className="sr-only">Use Microphone</span>
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent side="top">Use Microphone</TooltipContent>
+            </Tooltip>
+            <Button type="submit" size="sm" className="ml-auto gap-1.5">
+              Send Message
+              <CornerDownLeft className="size-3.5" />
+            </Button>
+          </div>
+          </TooltipProvider>
         </div>
-    );
-};
-
-export default page;
+        <div className="flex-1" >
+        ss
+        </div>
+      </div>
+    )
+  }
+  
